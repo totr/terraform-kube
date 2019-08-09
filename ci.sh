e5938d44b2a28758c758908f4569bd6ce2c5b5ce
Tomas Trnka <tt@tomastrnka.net>
2019-08-09 21:08:59 +0200

    added concourse pipeline

---
+++ ci.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+extra_args=""
+if [ "$1" == "up" ]; then
+	extra_args="-d --force-recreate"
+fi
+
+docker-compose -f concourse-ci.yml $1 $extra_args
+
+if [ "$1" == "up" ]; then
+	echo ""
+	echo "----------------------------------------------------------------"
+	echo "Concourse CI is running on http://localhost:8080 (admin / admin)"
+	echo "----------------------------------------------------------------"
+fi
