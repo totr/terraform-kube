162984b6462dca6730673faf0687f1bb28c9a1e4
Ondrej Michalcik <ondrej.michalcik@stratox.cz>
2021-03-24 18:00:04 +0100

    fix pipelines

--- provider/managed-k8s/digitalocean/main.tf
+++ provider/managed-k8s/digitalocean/main.tf
@@ -27,7 +27,7 @@ resource "digitalocean_kubernetes_cluster" "default" {
   name     = format("%s-%s", var.project_name, var.cluster_name)
   region   = var.provider_region
   version  = var.provider_k8s_version
-  vpc_uuid = digitalocean_vpc.get.id
+  vpc_uuid = data.digitalocean_vpc.get.id
 
   node_pool {
     name       = "application-pool"
@@ -55,7 +55,7 @@ resource "digitalocean_loadbalancer" "public" {
   name        = format("%s-%s-%s", var.project_name, var.cluster_name, "load-balancer")
   region      = var.provider_region
   droplet_tag = var.project_name
-  vpc_uuid    = digitalocean_vpc.get.id
+  vpc_uuid    = data.digitalocean_vpc.get.id
 
   forwarding_rule {
     entry_port      = 80
